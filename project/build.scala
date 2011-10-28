dd318cc4496b0872ead74d6bda02d54a89146b0b
Takahito Nakano <kano@ponko2.jp>
2011-10-28 21:53:32 +0900

    buildの設定を変更

--- project/build.scala
+++ project/build.scala
@@ -1,38 +1,77 @@
 import sbt._
-
 import Keys._
 import AndroidKeys._
 
-object General {
-  val settings = Defaults.defaultSettings ++ Seq (
-    name := "Web IME Mushroom",
-    version := "0.5.0",
-    scalaVersion := "2.9.1",
-    platformName in Android := "android-7"
+object BuildSettings {
+  val buildOrganization = "ponko2"
+  val buildVersion      = "0.5.0"
+  val buildScalaVersion = "2.9.1"
+  val buildPlatformName = "android-7"
+  val buildKeyalias     = "ponko2"
+
+  val buildSettings = Defaults.defaultSettings ++ Seq (
+    organization := buildOrganization,
+    version      := buildVersion,
+    scalaVersion := buildScalaVersion,
+    shellPrompt  := ShellPrompt.buildShellPrompt,
+    platformName in Android := buildPlatformName
   )
 
-  lazy val fullAndroidSettings =
-    General.settings ++
-    AndroidProject.androidSettings ++
+  val androidSettings = AndroidProject.androidSettings ++
     TypedResources.settings ++
-    AndroidMarketPublish.settings ++ Seq (
-      keyalias in Android := "ponko2",
-      libraryDependencies ++= Seq("org.scalatest"  %% "scalatest" % "1.6.1" % "test",
-                                  "net.databinder" %% "dispatch-http" % "0.7.8",
-                                  "net.databinder" %% "dispatch-lift-json" % "0.7.8")
-    )
+    AndroidMarketPublish.settings ++
+    Seq(keyalias in Android := buildKeyalias)
+}
+
+object ShellPrompt {
+  object devnull extends ProcessLogger {
+    def info  (s: => String) {}
+    def error (s: => String) {}
+    def buffer[T] (f: => T): T = f
+  }
+
+  def currBranch = (
+    ("git status -sb" lines_! devnull headOption)
+      getOrElse "-" stripPrefix "## "
+  )
+
+  val buildShellPrompt = {
+    (state: State) => {
+      val currProject = Project.extract (state).currentProject.id
+      "%s:%s:%s> ".format (
+        currProject, currBranch, BuildSettings.buildVersion
+      )
+    }
+  }
+}
+
+object Dependencies {
+  object Dispatch {
+    val http     = "net.databinder" %% "dispatch-http"      % "0.7.8"
+    val liftJson = "net.databinder" %% "dispatch-lift-json" % "0.7.8"
+  }
+
+  val scalatest = "org.scalatest" %% "scalatest" % "1.6.1" % "test"
 }
 
 object AndroidBuild extends Build {
+  import Dependencies._
+  import BuildSettings._
+
+  val mainDeps  = Seq(Dispatch.http, Dispatch.liftJson, scalatest)
+  val testsDeps = Seq(scalatest)
+
   lazy val main = Project (
     "Web IME Mushroom",
     file("."),
-    settings = General.fullAndroidSettings
-  )
+    settings = buildSettings ++ BuildSettings.androidSettings ++
+      Seq(libraryDependencies ++= mainDeps)
+  ) dependsOn (tests)
 
   lazy val tests = Project (
     "tests",
     file("tests"),
-    settings = General.settings ++ AndroidTest.androidSettings
-  ) dependsOn main
+    settings = buildSettings ++ AndroidTest.androidSettings ++
+      Seq(libraryDependencies ++= testsDeps)
+  )
 }
